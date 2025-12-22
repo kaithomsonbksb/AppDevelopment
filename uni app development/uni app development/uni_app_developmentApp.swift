2407b7dd9f772e9ef3cb8803daff10d861feaa9f
kai thomson <kai.thomson@vodafone.com>
2025-12-22 13:01:10 +0000

    first commit

---
+++ uni app development/uni app development/uni_app_developmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  uni_app_developmentApp.swift
+//  uni app development
+//
+//  Created by "Kai Thomson, Vodafone" on 22/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct uni_app_developmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
